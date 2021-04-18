f94b9344b99106fdd6cc2a7f0057f41cd8a27ecc
Thomas van der Plas <thomasvanderplas.leiden@gmail.com>
2021-04-18 15:27:57 +0200

    ,

--- HW2/public/scripts/report.js
+++ HW2/public/scripts/report.js
@@ -151,6 +151,7 @@ function Logout() {
     var allCookies = document.cookie.split(';');
     for (var i = 0; i < allCookies.length; i++) {
         if (allCookies[i].slice(0, 2) == "sk" || allCookies[i].slice(0, 2) == "un") {
+            console.log(allCookies[i].slice(0, 2))
             document.cookie = allCookies[i] + "=;expires=" + new Date(0).toUTCString();
         } else {
             document.cookie = allCookies[i]
