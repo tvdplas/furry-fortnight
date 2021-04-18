ddd774054b9263e093f322c65f3130467f8854f1
Thomas van der Plas <thomasvanderplas.leiden@gmail.com>
2021-04-18 12:29:07 +0200

    moved script into seperate files

---
+++ HW2/public/scripts/login.js
@@ -0,0 +1,36 @@
+function Login() {
+    let un = document.getElementById("usernameinp").value
+    let pw = document.getElementById("pwinp").value
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", '/login/', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState == 4) {
+
+            console.log(xhr.responseText)
+            let res = JSON.parse(xhr.responseText)
+            if (res.errcode == 300) {
+                window.location.replace(res.redirect)
+            }
+            document.getElementById("errmsg")?.remove();
+            var node = document.createElement("H2");
+            node.id = "errmsg"
+            var textnode
+
+            if (res.errcode == 1) {
+                textnode = document.createTextNode("Invalid login.");
+                node.appendChild(textnode);
+                document.getElementById("control-box").appendChild(node);
+            }
+            else if (res.loggedIn) {
+                window.location.replace(res.redirect)
+            }
+
+        }
+    }
+    xhr.send(JSON.stringify({
+        un: un,
+        pw: pw
+    }));
+}
